243fb000a54fc4d25894f863500419f91defc380
devyumao <yuzhang.lille@gmail.com>
2021-09-30 11:42:12 +0800

    feat: init

---
+++ src/game.ts
@@ -0,0 +1,47 @@
+import 'phaser';
+
+export default class Demo extends Phaser.Scene
+{
+    constructor ()
+    {
+        super('demo');
+    }
+
+    preload ()
+    {
+        this.load.image('logo', 'assets/phaser3-logo.png');
+        this.load.image('libs', 'assets/libs.png');
+        this.load.glsl('bundle', 'assets/plasma-bundle.glsl.js');
+        this.load.glsl('stars', 'assets/starfields.glsl.js');
+    }
+
+    create ()
+    {
+        this.add.shader('RGB Shift Field', 0, 0, 800, 600).setOrigin(0);
+
+        this.add.shader('Plasma', 0, 412, 800, 172).setOrigin(0);
+
+        this.add.image(400, 300, 'libs');
+
+        const logo = this.add.image(400, 70, 'logo');
+
+        this.tweens.add({
+            targets: logo,
+            y: 350,
+            duration: 1500,
+            ease: 'Sine.inOut',
+            yoyo: true,
+            repeat: -1
+        })
+    }
+}
+
+const config = {
+    type: Phaser.AUTO,
+    backgroundColor: '#125555',
+    width: 800,
+    height: 600,
+    scene: Demo
+};
+
+const game = new Phaser.Game(config);
